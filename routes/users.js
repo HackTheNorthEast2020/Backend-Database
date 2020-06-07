28d7b14418f16a93ce5a2d144c7d04949d83b486
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 02:11:06 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,9 @@ router.post(
   ],
   (req, res) => {
     const errors = validationResults(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
   }
 );
 
