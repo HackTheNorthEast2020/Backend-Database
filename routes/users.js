7f74540d63187d3f1d2d56983793757bcf0923da
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 02:12:56 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResults } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -18,10 +18,12 @@ router.post(
     ).isLength({ min: 6 }),
   ],
   (req, res) => {
-    const errors = validationResults(req);
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    res.send("passed");
   }
 );
 
