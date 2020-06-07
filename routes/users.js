0b4a6235c7e45078e0df2ca2dcdccc48f7af8941
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 02:23:23 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -42,6 +42,8 @@ router.post(
 
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send("User saved");
     } catch (error) {}
   }
 );
