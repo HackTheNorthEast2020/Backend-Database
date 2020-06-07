c191cc5bbe4e8f43a9553ecde2d24853ffc5a3a3
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 00:56:19 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,13 @@ const router = express.Router();
 // @route  GET api/auth
 // @desc Get logged in user
 // @access Private
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Register a user");
 });
 
 // @route  POST api/auth
-// @desc Get logged in user
-// @access Private
+// @desc Auth user and get token
+// @access Public
 router.post("/", (req, res) => {
   res.send("Register a user");
 });
