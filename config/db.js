b356ce553c03fa617a3a4125ee45c78f04663b3e
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 01:21:10 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,11 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = () => {
-  mongoose.connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB connected"));
 };
