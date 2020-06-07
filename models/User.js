5f18cefd785f37ad95043cb2bf99c846688bf754
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 01:29:50 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,4 +5,20 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  name: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
 });
