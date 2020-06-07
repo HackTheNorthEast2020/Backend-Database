35cbf8452c5e6c2ff0661f894b2a327e2f3fcf96
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 01:01:27 -0400

    Update goals.js

--- routes/goals.js
+++ routes/goals.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// @route  POST api/users
+// @route  POST api/goals
 // @desc Register a user
 // @access Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
+router.get("/", (req, res) => {
+  res.send("Get all goals");
 });
 
 module.exports = router;
