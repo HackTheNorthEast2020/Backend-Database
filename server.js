be5933a6672aa39bada11ac3bd78acefed43c379
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 00:59:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ app.get("/", (req, res) => res.json({ msg: " Welcome to the DreamR API" }));
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/goals", require("./routes/goals"));
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
