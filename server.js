61797f836a1b942dfe1d34ae010c117d64ce8a10
Syndicate555 <saziz028@uottawa.ca>
2020-06-07 00:36:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.json({msg: " Welcome to the DreamR API"});
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
